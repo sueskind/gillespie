a93b26c4e60799b6f397750c4120be60909268d5
sueskind <52210599+sueskind@users.noreply.github.com>
2021-03-14 16:09:32 +0100

    Simulation mask

---
+++ gillespie/simulation.py
@@ -0,0 +1,28 @@
+def run(initials, propensities, stoichiometry, duration, padding=False):
+    """
+    
+
+    :param initials:
+    :param propensities:
+    :param stoichiometry:
+    :param duration:
+    :param padding:
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    N = 50  # whole population
+    beta = 2  # transmission rate
+    gamma = 0.5  # recovery rate
+    t = 15  # duration
+
+    initials = [47, 3, 0]  # S, I, R
+
+    propensities = [lambda s, i, r: beta * s * i / N,  # S -> I, Propensity: b * S(t) * I(t) / N
+                    lambda s, i, r: gamma * i]  # I -> R Propensity: g*I(t)
+
+    stoichiometry = [[-1, 1, 0],  # S -> I, Population change: S-1, I+1, R+0
+                     [0, -1, 1]]  # I -> R Population change: S+0, I-1, R+1
+
+    run(initials, propensities, stoichiometry, t, padding=True)
